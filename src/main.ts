1552a0871dfac332101ea95b463d19c222690cb5
lonewolfyx <877085893@qq.com>
2025-01-13 22:36:04 +0800

    init: 初始化 Arco Vue 项目模板

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue';
+import ArcoVue from '@arco-design/web-vue';
+
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import '@arco-design/web-vue/dist/arco.css';
+import './assets/reset.css';
+import './assets/tailwind.css';
+
+import './assets/main.css';
+
+import router from './router';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(ArcoVue);
+
+app.mount('#app');
