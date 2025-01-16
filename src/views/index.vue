c60359c5a6b5b8cc8077d6b72c5e6a37b9263388
lonewolfyx <877085893@qq.com>
2025-01-16 22:47:46 +0800

    feat: 新增默认文件

---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex">default page info</div>
+
+    {{ userInfo }}
+</template>
+
+<script setup lang="ts">
+import {useUserStore} from "@/stores";
+import {ref} from "vue";
+
+console.log(import.meta.env);
+
+const userStore = useUserStore()
+const userInfo = ref(userStore.userInfo)
+</script>
+
+<style scoped></style>
