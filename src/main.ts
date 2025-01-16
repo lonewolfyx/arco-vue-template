8cc5b436a4c959123525cd704361f663e58940d1
lonewolfyx <877085893@qq.com>
2025-01-16 22:37:32 +0800

    feat: 新增 Arco Icon

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import ArcoVue from '@arco-design/web-vue';
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 
-import { createPinia } from 'pinia';
+import {createPinia} from 'pinia';
 
 import App from './App.vue';
 import '@arco-design/web-vue/dist/arco.css';
@@ -17,5 +18,6 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(ArcoVue);
+app.use(ArcoVueIcon)
 
 app.mount('#app');
