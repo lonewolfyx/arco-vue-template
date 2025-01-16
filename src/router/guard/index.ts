5857400e1a7d667899e6f410f274d2d80d0193e3
lonewolfyx <877085893@qq.com>
2025-01-16 22:46:46 +0800

    feat: 新增注释

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -1,6 +1,10 @@
 import type {Router} from 'vue-router';
 import {setupUserLoginInfoGuard} from "@/router/guard/userLoginInfo";
 
+/**
+ * 创建路由守卫
+ * @param router
+ */
 export const createRouteGuard = (router: Router) => {
     setupUserLoginInfoGuard(router)
 }
