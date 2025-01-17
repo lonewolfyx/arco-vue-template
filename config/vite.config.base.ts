9bb93889e998a3acb8106546bf1557ed32494001
lonewolfyx <877085893@qq.com>
2025-01-17 13:32:37 +0800

    feat: 新增自动导入 vue pinia 依赖

--- config/vite.config.base.ts
+++ config/vite.config.base.ts
@@ -3,12 +3,22 @@ import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import {vitePluginForArco} from "@arco-plugins/vite-vue";
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
     plugins: [
         vue(),
         vueJsx(),
-        vitePluginForArco()
+        vitePluginForArco(),
+        AutoImport({
+            // resolvers: [ArcoResolver()],
+            imports: [
+                'vue',
+                'pinia',
+                '@vueuse/core'
+            ],
+            dts: 'src/types/auto-imports.d.ts',
+        }),
     ],
     resolve: {
         alias: [
