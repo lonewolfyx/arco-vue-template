643f74033911d6ea7ca9c638386c7a0eb1c756d7
lonewolfyx <877085893@qq.com>
2025-01-20 22:22:19 +0800

    docs: 删除注释

--- src/utils/request/plugins/set.header.token.ts
+++ src/utils/request/plugins/set.header.token.ts
@@ -10,7 +10,6 @@ export const setHeaderToken = <T extends InternalAxiosRequestConfig<any>>(
     config: T,
     isToken: boolean
 ): T => {
-    console.log('isToken', isToken);
     if (getToken() && !isToken) {
         config.headers['Authorization'] = 'Bearer ' + getToken(); // 让每个请求携带自定义token 请根据实际情况自行修改
     }
